669dd6273777fb109464c1b8172a2bd890a17526
Agustin Vidable <agustin.vidable@hotmail.com.ar>
2025-09-18 21:56:22 -0300

    se agrego la funcionalidad del boton

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+let boton=document.getElementById("up");
+
+window.onscroll=function(){
+    if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+        boton.style.display="block";
+    }else{
+        boton.style.display="none";
+    }
+}
+
 function botonup(){
     window.scrollTo({
         top: 0,
